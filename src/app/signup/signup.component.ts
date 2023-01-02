0c9f0861ce0b36d0dfbaace5af0e95631b0a899f
nonyereeta <nonyeibeanu@gmail.com>
2023-01-02 09:50:23 +0100

    update to signup ui and component

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, Validators, FormGroup} from '@angular/forms';
 import { ApiService } from 'src/app/services/api.service';
 import { Router } from '@angular/router';
+import { catchError, first, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
@@ -33,21 +34,32 @@ export class SignupComponent implements OnInit {
   }
 
   signup() {
-    this.apiService.setLoadingStatus(true);
+    this.apiService.setLoadingStatus(true);     
+    this.apiService.userSignup(this.registerForm.value).pipe(catchError((error) => {
+    this.error = 'User already exist'
+    this.apiService.setLoadingStatus(false);
+    return throwError(error);
+    }))
+    .subscribe((res) => {
+      console.log(res)
+    this.apiService.setUserData(res.user);
+    this.error = '';      
+    this.router.navigate([''])
+    this.apiService.setLoadingStatus(false);
+  });
 
-    try{
-     
-    this.apiService.userSignup(this.registerForm.value).subscribe((res) => {
-        this.apiService.setUserData(res.user);
-        // this.router.navigate([''])
-        this.error = '';      
-      this.apiService.setLoadingStatus(false);
-    });
-    }
-    catch(error) {
-        this.error = "User already registered"
-        this.apiService.setLoadingStatus(false);
-    }
-
+  // this.apiService.userSignup(this.registerForm.value).pipe(first())
+  // .subscribe(res => {
+  //       console.log(res)
+  //     this.apiService.setUserData(res.user);
+  //     this.router.navigate([''])
+  //     this.error = '';      
+  //     this.apiService.setLoadingStatus(false);
+  // },
+  // error => {
+  //     this.error = 'User already exist'
+  //   this.apiService.setLoadingStatus(false);
+  // }
+  // )
   }
 }
