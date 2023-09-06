5d7805a43ba3eb92d292bf1a8c7713bed3984d15
Nonye Ibeanu <nonyeibeanu@gmail.com>
2023-09-06 17:42:38 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   API_BASE: 'https://nonye-ibeanu-blogging-api.cyclic.app',
   // API_BASE: 'http://localhost:3000'
 };
